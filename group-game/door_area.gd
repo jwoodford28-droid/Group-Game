ac137be67537512d21c1dea2bea809d4e53f0a60
pdewilliams28-art <pdewilliams28@bfhsla.org>
2026-05-05 15:30:39 -0500

    Puzzle

---
+++ group-game/door_area.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+@export var button_presses_needed: int = 3
+var buttons_pressed: int = 0
+
+
+func _on_puzzle_button_pressed():
+	buttons_pressed += 1
+	if buttons_pressed >= button_presses_needed:
+		visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
+		
+		
+		
+		
+func _on_puzzle_button_unpressed():
+	buttons_pressed -= 1
+	if buttons_pressed < button_presses_needed:
+		visible = true
+		$CollisionShape2D.set_deferred("disabled", false)
